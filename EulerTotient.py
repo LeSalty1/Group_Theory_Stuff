6d578a70a11ba08e5f961d84dbe4843e17855689
SaltyArjun <endogenousvar@gmail.com>
2026-07-17 10:12:47 -0400

    Fixed Euler totient function (slightly better than naive appraoch).

--- EulerTotient.py
+++ EulerTotient.py
@@ -1,6 +1,6 @@
 import math
 from fractions import Fraction
-# STILL A WORK IN PROGRESS
+
 # Avoiding sympy because it already has an Euler totient function. 
 def is_prime(candidate): 
     for numb in range(2, int(candidate**0.5) + 1): 
@@ -9,15 +9,14 @@ def is_prime(candidate):
     return True
 
 def euler_totient(n):
-    result = Fraction(n, 1)
     if n == 1: 
         return 1
-    primes = []
-    for test in range(2, int(n**0.5) + 1): 
-        if is_prime(test) and n % test == 0: 
-            primes.append(test)
+    if is_prime(n): 
+        return n - 1 # The fact that phi(n) = n - 1 if n is prime should be clear. 
+    primes = [] 
+    for numb in range(2, n//2 + 1): 
+        if is_prime(numb) and n % numb == 0: 
+            primes.append(numb)
     for prime in primes: 
-        result *= (1 - Fraction(1, prime))
-    return int(result)
-for i in range(2,51): 
-    print(i, euler_totient(i))
+        n *= 1 - Fraction(1, prime)
+    return n
